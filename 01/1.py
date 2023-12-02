9503ae8f6f16ec93be8a8b15cf6c9a6c59333a0e
Giovanni Go <dreamnid@dreamnid.com>
2023-12-02 12:26:34 -0800

    Finish day 1

--- 01/1.py
+++ 01/1.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from collections import defaultdict
+from collections.abc import Callable, Collection, Iterable
 from functools import partial, reduce
 from itertools import chain, cycle, takewhile
 import math
@@ -8,7 +9,6 @@ import os
 import pprint
 import re
 from time import time
-from typing import Dict, List, Set, Tuple, Union
 
 from humanize import intcomma
 
@@ -23,9 +23,56 @@ if __name__ == '__main__':
         from util import *
 
 INPUT_FILE='1-input.txt'
-INPUT_FILE='1a-example.txt'
+#INPUT_FILE='1a-example.txt'
+#INPUT_FILE='1b-example.txt'
 
-re_expr = r'$\w*?(\d+).*?(\d+)\w*?^'
-input = [line for line in get_file_contents(INPUT_FILE)[0]]
+RE_DIGIT = re.compile(r'\d')
 
-print(input)
+def convert_word_with_digit(mystr: str):
+    """
+    Return the numerical digit of the starting word
+
+    E.g.
+    two3eight will return '2' (the string)
+    atwo3eight will return None
+    """
+    res = mystr
+
+    # assume myStr has the same casing
+    trans_table = (('zero', '0'),
+                   ('one', '1'),
+                   ('two', '2'),
+                   ('three', '3'),
+                   ('four', '4'),
+                   ('five', '5'),
+                   ('six', '6'),
+                   ('seven', '7'),
+                   ('eight', '8'),
+                   ('nine', '9'),
+                  )
+
+    for trans in trans_table:
+        if mystr.startswith(trans[0]):
+            return trans[1]
+
+    return None
+
+
+def get_calibration_values(parse_digits=False):
+    nums = []
+    for line in get_file_contents(INPUT_FILE)[0]:
+        buf = ''
+        for i, cur_char in enumerate(line):
+            if parse_digits:
+                if (word_to_digit := convert_word_with_digit(line[i:])):
+                    cur_char = word_to_digit
+
+            if RE_DIGIT.match(cur_char):
+                buf += cur_char
+
+        if len(buf) > 0:
+            nums.append(int(buf[0]+buf[-1]))
+    return nums
+
+print('1:', sum(get_calibration_values()))
+print('2:', sum(get_calibration_values(parse_digits=True)))
--- 01/README.md
+++ 01/README.md
@@ -1,6 +1,27 @@
 # Day 1
 
+## Issues encountered
+### Common letters
+I thought it wanted to prioritize the first digit word
+
+But actually, common letters can be for either case.
+
+### Lines with fewer than 2 numbers
+The puzzle clearly states the calibration value is 2 digits, but does not clarify if the line only contains 1 numerical value. 
+Apparently, if there is only one number, it should be used twice.
+
+## Tricky test cases
+
+```
+1eightwo -> 12
+beightwo4 -> 84
+```
+
+
 ## Performance
 ```bash
-
+(adventofcode) ➜  01 git:(main) ✗ time python 1.py 
+1: 56049
+2: 54530
+python 1.py  0.05s user 0.01s system 98% cpu 0.063 total
 ```
