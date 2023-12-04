96630e5773afd5bc16ae8199a24c55629d4859f8
Giovanni Go <dreamnid@dreamnid.com>
2023-12-04 01:33:13 +0000

    Day 3 part 1

--- 03/3.py
+++ 03/3.py
@@ -9,11 +9,10 @@ import pprint
 import re
 from time import time
 
-from humanize import intcomma
 
 # Fix path so we can do a relative import: https://stackoverflow.com/a/27876800
 if __name__ == '__main__':
-    if __package__ is None:
+    if not __package__:
         import sys
         from os import path
         sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
@@ -22,6 +21,39 @@ if __name__ == '__main__':
         from util import *
 
 INPUT_FILE='3-input.txt'
-INPUT_FILE='3a-example.txt'
+#INPUT_FILE='3a-example.txt'
+#INPUT_FILE='3a-test1.txt'
 
 input = [line for line in get_file_contents(INPUT_FILE)[0]]
+
+RE_DIGIT = re.compile(r'\d')
+SYMBOL = re.compile(r'[^\w\s.]') # Exclude non word/space char and periods
+
+print([SYMBOL.match(x) for x in ['/', '*', '&']])
+
+nums = []
+for row, line in enumerate(input):
+    cur_buf = ''
+    is_valid = False
+    for col, cur_char in enumerate(line):
+        if RE_DIGIT.match(cur_char):
+            cur_buf += cur_char
+            is_valid |= any([SYMBOL.match(x) for x in get_neighbors(input, row, col)])
+        else:
+            if cur_buf:
+                if is_valid:
+                    nums.append(int(cur_buf))
+                cur_buf = ''
+                is_valid = False
+        # print(cur_char, get_neighbors(input, row, col),[SYMBOL.match(x) for x in get_neighbors(input, row, col)]) 
+
+    # Handle last column has a valid number
+    if cur_buf and is_valid:
+        nums.append(int(cur_buf))
+
+    # print(row+1, nums)
+    # nums = []
+    # print('----')
+
+#print(nums)
+print('1:', sum(nums))
