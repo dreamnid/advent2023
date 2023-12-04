2d7ff98af021a7878a3e27af0c2a39af8d0f2971
Giovanni Go <dreamnid@dreamnid.com>
2023-12-04 03:10:15 +0000

    Add day 3 part 2

--- 03/3.py
+++ 03/3.py
@@ -29,28 +29,41 @@ input = [line for line in get_file_contents(INPUT_FILE)[0]]
 RE_DIGIT = re.compile(r'\d')
 SYMBOL = re.compile(r'[^\w\s.]') # Exclude non word/space char and periods
 
+star: dict[tuple[int, int], list[int]] = defaultdict(list)
+
 nums = []
 for row, line in enumerate(input):
     cur_buf = ''
     is_valid = False
+    neighboring_star_positions: set[tuple[int, int]] = set()
     for col, cur_char in enumerate(line):
         if RE_DIGIT.match(cur_char):
             cur_buf += cur_char
             is_valid |= any([SYMBOL.match(x) for x in get_neighbors(input, row, col)])
+            neighboring_star_positions |= {x[1] for x in get_neighbors_with_pos(input, row, col) if x[0] == '*'}
         else:
             if cur_buf:
                 if is_valid:
                     nums.append(int(cur_buf))
+                if neighboring_star_positions:
+                    for cur_star_pos in neighboring_star_positions:
+                        star[cur_star_pos].append(int(cur_buf))
                 cur_buf = ''
                 is_valid = False
+                neighboring_star_positions = set()
         # print(cur_char, get_neighbors(input, row, col),[SYMBOL.match(x) for x in get_neighbors(input, row, col)]) 
 
-    # Handle last column has a valid number
+    # Handle last column that has a valid number
     if cur_buf and is_valid:
         nums.append(int(cur_buf))
+
+    if cur_buf and neighboring_star_positions:
+        for cur_star_pos in neighboring_star_positions:
+            star[cur_star_pos].append(int(cur_buf))
 
     # print(row+1, nums)
     # nums = []
     # print('----')
 
 print('1:', sum(nums))
+print('2:', sum([reduce(mul, j) for j in star.values() if len(j) == 2]))
--- 03/README.md
+++ 03/README.md
@@ -1,6 +1,16 @@
 # Day 3
+Some issues encountered:
+* Did not know if need to dedupe part numbers (answer: no)
+* Need to run number logic if it is stored in the last column of the row 
+* Accidentally changed input file during troubleshooting
 
 ## Performance
 ```bash
+time python 3.py
+1: 522726
+2: 81721933
 
+real    0m0.060s
+user    0m0.048s
+sys     0m0.012s
 ```
