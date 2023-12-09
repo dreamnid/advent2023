a47dad484d9ba9c3670478757ba2f8b488eecca3
Giovanni Go <dreamnid@dreamnid.com>
2023-12-09 06:08:49 +0000

    Add day 9

---
+++ 09/9a.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+from collections import defaultdict
+from functools import partial, reduce
+from itertools import chain, cycle, takewhile
+import math
+from operator import mul, itemgetter
+import os
+import pprint
+import re
+from time import time
+
+from humanize import intcomma
+
+# Fix path so we can do a relative import: https://stackoverflow.com/a/27876800
+if __name__ == '__main__':
+    if not __package__:
+        import sys
+        from os import path
+        sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
+        # Relative imports here
+        from util import *
+
+INPUT_FILE='9-input.txt'
+# INPUT_FILE='9a-example.txt'
+
+input = [list(map(lambda x: int(x), line.split(' '))) for line in get_file_contents(INPUT_FILE)[0]]
+
+overall = []
+sums = []
+for history_idx, history in enumerate(input):
+    cur_stack = history
+    overall.append([history])
+    sums.append(history[-1])
+    while any([a != 0 for a in cur_stack]):
+        next_stack = [cur_stack[i+1] - cur_stack[i] for i in range(len(cur_stack)-1)]
+        overall[history_idx].append(next_stack)
+        cur_stack = next_stack
+        sums[history_idx] += next_stack[-1]
+
+print('1:', sum(sums))
---
+++ 09/README.md
@@ -0,0 +1,18 @@
+# Day 9
+
+## Performance
+```bash
+(.venv) @dreamnid ➜ /workspaces/advent2023/09 (main) $ time python 9a.py
+1: 1980437560
+
+real    0m0.087s
+user    0m0.062s
+sys     0m0.025s
+
+(.venv) @dreamnid ➜ /workspaces/advent2023/09 (main) $ time python 9b.py
+1: 977
+
+real    0m0.087s
+user    0m0.073s
+sys     0m0.013s
+```
