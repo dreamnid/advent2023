c9fddf3ba370b0ec0fcf366d3d5bf435e2740af5
Giovanni Go <dreamnid@dreamnid.com>
2023-12-16 08:07:24 +0000

    Add Day 16 part 2

--- 16/16.py
+++ 16/16.py
@@ -26,6 +26,8 @@ INPUT_FILE='16-input.txt'
 # INPUT_FILE='16a-example.txt'
 
 input = [line for line in get_file_contents(INPUT_FILE)[0]]
+orig_rows_count = len(input)
+orig_cols_count = len(input[0])
 
 input = add_padding(input, 'X')
 
@@ -38,96 +40,116 @@ class Dir(Enum):
 type Pos = list[int, int]
 type Node = list[Pos, Dir]
 
-visited: set[Pos] = set()
-visited_with_dir: set[Node] = set()
 
-start_pos = [[1, 1], Dir.RIGHT]
+def solver(start_pos: Node):
+    visited: set[Pos] = set()
+    visited_with_dir: set[Node] = set()
+    process = [start_pos]
 
-process = [start_pos]
+    idx = 0
+    while (process):
+        cur_node = process.pop(0)
+        cur_row, cur_col = cur_node[0]
+        # print(cur_node, input[cur_row][cur_col])
 
-idx = 0
-while (process):
-    cur_node = process.pop(0)
-    cur_row, cur_col = cur_node[0]
-    # print(cur_node, input[cur_row][cur_col])
-
-    if (tuple(cur_node[0]), cur_node[1],) in visited_with_dir:
-        continue
-
-    cur_dir = cur_node[1]
-
-    match(input[cur_row][cur_col]):
-        case '.':
-            match cur_dir:
-                case Dir.RIGHT:
-                    process.append([[cur_row, cur_col + 1], Dir.RIGHT])
-                case Dir.UP:
-                    process.append([[cur_row - 1, cur_col], Dir.UP])
-                case Dir.LEFT:
-                    process.append([[cur_row, cur_col - 1], Dir.LEFT])
-                case Dir.DOWN:
-                    process.append([[cur_row + 1, cur_col], Dir.DOWN])
-        case '\\':
-            match cur_dir:
-                case Dir.RIGHT:
-                    process.append([[cur_row + 1, cur_col], Dir.DOWN])
-                case Dir.UP:
-                    process.append([[cur_row , cur_col - 1], Dir.LEFT])
-                case Dir.LEFT:
-                    process.append([[cur_row - 1, cur_col], Dir.UP])
-                case Dir.DOWN:
-                    process.append([[cur_row, cur_col + 1], Dir.RIGHT])
-        case '|':
-            match cur_dir:
-                case Dir.RIGHT:
-                    process.append([[cur_row - 1, cur_col], Dir.UP])
-                    process.append([[cur_row + 1, cur_col], Dir.DOWN])
-                case Dir.UP:
-                    process.append([[cur_row - 1, cur_col], Dir.UP])
-                case Dir.LEFT:
-                    process.append([[cur_row - 1, cur_col], Dir.UP])
-                    process.append([[cur_row + 1, cur_col], Dir.DOWN])
-                case Dir.DOWN:
-                    process.append([[cur_row + 1, cur_col], Dir.DOWN])
-        case '/':
-            match cur_dir:
-                case Dir.RIGHT:
-                    process.append([[cur_row - 1, cur_col], Dir.UP])
-                case Dir.UP:
-                    process.append([[cur_row, cur_col + 1], Dir.RIGHT])
-                case Dir.LEFT:
-                    process.append([[cur_row + 1, cur_col], Dir.DOWN])
-                case Dir.DOWN:
-                    process.append([[cur_row, cur_col - 1], Dir.LEFT])
-        case '-':
-            match cur_dir:
-                case Dir.RIGHT:
-                    process.append([[cur_row, cur_col + 1], Dir.RIGHT])
-                case Dir.UP:
-                    process.append([[cur_row, cur_col - 1], Dir.LEFT])
-                    process.append([[cur_row, cur_col + 1], Dir.RIGHT])
-                case Dir.LEFT:
-                    process.append([[cur_row, cur_col - 1], Dir.LEFT])
-                case Dir.DOWN:
-                    process.append([[cur_row, cur_col - 1], Dir.LEFT])
-                    process.append([[cur_row, cur_col + 1], Dir.RIGHT])
-        case _:
+        if (tuple(cur_node[0]), cur_node[1],) in visited_with_dir:
             continue
-    
-    visited.add(tuple(cur_node[0]))
-    visited_with_dir.add((tuple(cur_node[0]), cur_node[1],))
 
-    if idx % 10 == 0 and False:
-        for row_idx, row in enumerate(input):
-            for col_idx, col in enumerate(row):
-                if (row_idx, col_idx) in visited:
-                    print('#', end='')
-                else:
-                    print('.', end='')
-            print()
+        cur_dir = cur_node[1]
+
+        match(input[cur_row][cur_col]):
+            case '.':
+                match cur_dir:
+                    case Dir.RIGHT:
+                        process.append([[cur_row, cur_col + 1], Dir.RIGHT])
+                    case Dir.UP:
+                        process.append([[cur_row - 1, cur_col], Dir.UP])
+                    case Dir.LEFT:
+                        process.append([[cur_row, cur_col - 1], Dir.LEFT])
+                    case Dir.DOWN:
+                        process.append([[cur_row + 1, cur_col], Dir.DOWN])
+            case '\\':
+                match cur_dir:
+                    case Dir.RIGHT:
+                        process.append([[cur_row + 1, cur_col], Dir.DOWN])
+                    case Dir.UP:
+                        process.append([[cur_row , cur_col - 1], Dir.LEFT])
+                    case Dir.LEFT:
+                        process.append([[cur_row - 1, cur_col], Dir.UP])
+                    case Dir.DOWN:
+                        process.append([[cur_row, cur_col + 1], Dir.RIGHT])
+            case '|':
+                match cur_dir:
+                    case Dir.RIGHT:
+                        process.append([[cur_row - 1, cur_col], Dir.UP])
+                        process.append([[cur_row + 1, cur_col], Dir.DOWN])
+                    case Dir.UP:
+                        process.append([[cur_row - 1, cur_col], Dir.UP])
+                    case Dir.LEFT:
+                        process.append([[cur_row - 1, cur_col], Dir.UP])
+                        process.append([[cur_row + 1, cur_col], Dir.DOWN])
+                    case Dir.DOWN:
+                        process.append([[cur_row + 1, cur_col], Dir.DOWN])
+            case '/':
+                match cur_dir:
+                    case Dir.RIGHT:
+                        process.append([[cur_row - 1, cur_col], Dir.UP])
+                    case Dir.UP:
+                        process.append([[cur_row, cur_col + 1], Dir.RIGHT])
+                    case Dir.LEFT:
+                        process.append([[cur_row + 1, cur_col], Dir.DOWN])
+                    case Dir.DOWN:
+                        process.append([[cur_row, cur_col - 1], Dir.LEFT])
+            case '-':
+                match cur_dir:
+                    case Dir.RIGHT:
+                        process.append([[cur_row, cur_col + 1], Dir.RIGHT])
+                    case Dir.UP:
+                        process.append([[cur_row, cur_col - 1], Dir.LEFT])
+                        process.append([[cur_row, cur_col + 1], Dir.RIGHT])
+                    case Dir.LEFT:
+                        process.append([[cur_row, cur_col - 1], Dir.LEFT])
+                    case Dir.DOWN:
+                        process.append([[cur_row, cur_col - 1], Dir.LEFT])
+                        process.append([[cur_row, cur_col + 1], Dir.RIGHT])
+            case _:
+                continue
         
-        print('---------------')
-    
-    idx += 1
+        visited.add(tuple(cur_node[0]))
+        visited_with_dir.add((tuple(cur_node[0]), cur_node[1],))
 
-print('1:', len(visited)) 
+        if idx % 10 == 0 and False:
+            for row_idx, row in enumerate(input):
+                for col_idx, col in enumerate(row):
+                    if (row_idx, col_idx) in visited:
+                        print('#', end='')
+                    else:
+                        print('.', end='')
+                print()
+            
+            print('---------------')
+        
+        idx += 1
+    return len(visited)
+
+print('1:', solver([[1, 1], Dir.RIGHT]))
+
+res = set()
+
+# Top row
+for i in range(1, orig_cols_count + 1):
+    res.add(solver(([1, i], Dir.DOWN)))
+
+# Bottom row
+for i in range(1, orig_cols_count + 1):
+    res.add(solver(([orig_rows_count, i], Dir.UP)))
+
+# Left col
+for i in range(1, orig_rows_count + 1):
+    res.add(solver(([i, 1], Dir.RIGHT)))
+
+# Right col
+for i in range(1, orig_rows_count + 1):
+    res.add(solver(([i, orig_cols_count], Dir.LEFT)))
+
+print('2:', max(res))
--- 16/README.md
+++ 16/README.md
@@ -2,5 +2,11 @@
 
 ## Performance
 ```bash
+@dreamnid ➜ /workspaces/advent2023/16 (main) $ time python 16.py
+1: 6816
+2: 8163
 
+real    0m3.529s
+user    0m3.338s
+sys     0m0.138s
 ```
