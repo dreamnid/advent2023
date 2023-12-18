4883a71af7d4967883fb7e1981de070a4b83db4b
Giovanni Go <dreamnid@dreamnid.com>
2023-12-18 11:07:35 +0000

    Day 17 part 2

---
+++ 17/17b.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python3
+from collections import defaultdict
+from collections.abc import Callable, Container, Sequence
+from enum import Enum
+from functools import partial, reduce
+from itertools import chain, cycle, takewhile
+import math
+from operator import mul, itemgetter
+import os
+import pprint
+import re
+from time import time
+from typing import NamedTuple
+
+from humanize import intcomma
+from termcolor import colored, cprint
+
+# Fix path so we can do a relative import: https://stackoverflow.com/a/27876800
+if __name__ == '__main__':
+    if not __package__:
+        import sys
+        from os import path
+        sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
+        # Relative imports here
+        from util import *
+
+INPUT_FILE='17-input.txt'
+# INPUT_FILE='17a-example.txt'
+# INPUT_FILE='17b-example.txt'
+
+input = [[int(x) for x in line] for line in get_file_contents(INPUT_FILE)[0]]
+import pandas as pd
+
+
+class Dir(Enum):
+    UP = 'UP'
+    DOWN = 'DOWN'
+    LEFT = 'LEFT'
+    RIGHT = 'RIGHT'
+
+
+class Pos(NamedTuple):
+    row_idx: int
+    col_idx: int
+
+
+class Movement(NamedTuple):
+    dir: Dir
+    same_dir_count: int
+
+
+class Node(NamedTuple):
+    pos: Pos
+    movement: Movement
+    dist: int = 0
+    prev: list[Pos] = []
+
+
+input = add_padding(input, float('inf'))
+
+start_pos = Pos(1, 1)
+finish_pos = Pos(len(input) - 2, len(input[0]) - 2)
+
+dist: dict[Pos, float | int] = defaultdict(lambda: float('inf'))
+pending_visit: set[tuple[Pos, Movement]] = set()
+visited: set[tuple[Pos, Movement]] = set()
+prev: dict[Pos, list[Pos]] = defaultdict(list)
+
+
+def is_same_movement_allowed(dir: Dir, movement: Movement):
+    if movement.dir == dir and movement.same_dir_count < MAX_SAME_DIR_COUNT:
+        return True
+
+    return False
+
+
+def get_val(pos: Pos, matrix):
+    return matrix[pos.row_idx][pos.col_idx]
+
+
+def is_valid_pos(pos: Pos, matrix):
+    return get_val(pos, matrix) != float('inf')
+
+
+def process_dir(cur_node: Node, next_dir: Dir, next_pos: Pos, matrix, visited):
+    next_movement_count = cur_node.movement.same_dir_count + 1 if cur_node.movement.dir == next_dir else 1
+    next_movement = Movement(next_dir, next_movement_count)
+    if is_valid_pos(next_pos, matrix) and (next_pos, next_movement) not in pending_visit:
+        if is_same_movement_allowed(next_dir, cur_node.movement) or (cur_node.movement.dir != next_dir and cur_node.movement.same_dir_count >= MIN_SAME_DIR_COUNT):
+            neighbor_distance = cur_node.dist + get_val(next_pos, matrix)
+            changed = False
+            if neighbor_distance <= dist[next_pos] and (next_dir == cur_node.movement.dir and cur_node.movement.same_dir_count >= MIN_SAME_DIR_COUNT):
+                dist[next_pos] = neighbor_distance
+                prev[next_pos] = cur_node.prev
+
+            pending_visit.add((next_pos, next_movement))
+            return Node(next_pos, next_movement, neighbor_distance, [*cur_node.prev, cur_node.pos])
+    
+    return None
+
+
+MIN_SAME_DIR_COUNT = 4
+MAX_SAME_DIR_COUNT = 10
+
+# Initialization
+# Set the distances of the neighbors to dist
+right_pos = Pos(row_idx=1, col_idx=2)
+down_pos = Pos(row_idx=2, col_idx=1)
+
+# Add neighbors of start_pos to q
+dist[right_pos] = get_val(right_pos, input)
+dist[down_pos] = get_val(down_pos, input)
+q: list[Node] = [Node(right_pos, Movement(Dir.RIGHT, 1), dist[right_pos]), Node(down_pos, Movement(Dir.DOWN, 1), dist[down_pos])]
+
+while q:
+    # q.sort(key=lambda cur_node: dist[cur_node.pos])
+    q.sort(key=lambda cur_node: cur_node.dist)
+
+    # Get the next node with the lowest heat cost
+    cur_node = q.pop(0)
+    visited.add((cur_node.pos, cur_node.movement))
+    
+    neighbors = []
+
+    # Up
+    if cur_node.movement.dir != Dir.DOWN:
+        if (next_node := process_dir(cur_node, Dir.UP, Pos(cur_node.pos.row_idx-1, cur_node.pos.col_idx), input, visited)):
+            neighbors.append(next_node)
+    # Left
+    if cur_node.movement.dir != Dir.RIGHT:
+        if (next_node := process_dir(cur_node, Dir.LEFT, Pos(cur_node.pos.row_idx, cur_node.pos.col_idx-1), input, visited)):
+            neighbors.append(next_node)
+    # Down
+    if cur_node.movement.dir != Dir.UP:
+        if (next_node := process_dir(cur_node, Dir.DOWN, Pos(cur_node.pos.row_idx+1, cur_node.pos.col_idx), input, visited)):
+            neighbors.append(next_node)
+    # Right
+    if cur_node.movement.dir != Dir.LEFT:
+        if (next_node := process_dir(cur_node, Dir.RIGHT, Pos(cur_node.pos.row_idx, cur_node.pos.col_idx+1), input, visited)):
+            neighbors.append(next_node)
+
+    if dist[finish_pos] != float('inf'):
+        print('early')
+        break
+
+    q.extend(neighbors) 
+
+print('2:', dist[finish_pos])
+
+# print(get_val(Pos(1, 1), input), is_valid_pos(Pos(1, 1), input))
+# print(pd.DataFrame(input))
+
+# Print traversal path
+# traversal_path = prev[finish_pos] 
+# pprint.pprint(traversal_path)
+# for row_idx, row in enumerate(input):
+#     for col_idx, col in enumerate(row):
+#         print(f'{colored(col, 'green')} ' if Pos(row_idx, col_idx) in traversal_path else f'{col} ', end='')
+#     print()
+
+#46
+
+# row 2, col 10 - 35 vs 32
+# pprint.pprint(dist)
--- 17/README.md
+++ 17/README.md
@@ -2,5 +2,18 @@
 
 ## Performance
 ```bash
+# Part 1
+(.venv) vscode ➜ /workspaces/2023/17 (main) $ time python 17a.py
+real    2m34.996s
+user    2m35.110s
+sys     0m1.541s
 
+# Part 2
+(.venv) vscode ➜ /workspaces/2023/17 (main) $ time python 17b.py
+early
+2: 1180
+
+real    3m9.414s
+user    3m9.342s
+sys     0m1.689s
 ```
