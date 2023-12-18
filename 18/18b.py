cb2f3e4eb3ebe1174eebb59a2f0c3778a0dbe99b
Giovanni Go <dreamnid@dreamnid.com>
2023-12-18 09:47:14 +0000

    Day 18 part 2

--- 18/18b.py
+++ 18/18b.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from collections import defaultdict
+from collections.abc import Sequence
 from enum import Enum 
 from functools import partial, reduce
 from itertools import chain, cycle, takewhile
@@ -41,15 +42,35 @@ class Pos(NamedTuple):
     col_idx: int
 
 
-land: dict[int, dict[int, str]] = defaultdict(lambda: defaultdict(lambda: '.'))
+def range_overlaps(row1_range: Sequence[int, int], row2_range: Sequence[int, int]):
+    if row1_range[0] < row2_range[0]:
+        row_a_range = row1_range
+        row_b_range = row2_range
+    else:
+        row_a_range = row2_range
+        row_b_range = row1_range
 
-cur_pos = Pos(0, 0)
-land[0][0] = '#'
+    # row_a_range covers row_b_range
+    if row_a_range[0] <= row_b_range[0] and row_a_range[1] >= row_b_range[0]:
+        return True
 
-col_idx_start = 0
-col_idx_end = 0
+    # row_b_range covers row_a_range
+    if row_a_range[0] >= row_b_range[0] and row_a_range[1] < row_b_range[0]:
+        return True
+
+    return False
+
+
+land: dict[int, list[tuple[int, int]]] = defaultdict(list)
+
 row_idx_start = 0
 row_idx_end = 0
+col_idx_start = 0
+col_idx_end = 0
+cur_pos = Pos(0, 0)
+cur_count = 0
+
+cur_stack_count = 1
 for line in input:
     dir_str, count_str, rgb = line.split(' ')
     dir = Dir(dir_str)
@@ -69,22 +90,27 @@ for line in input:
         
         count = int(rgb[1:-1], base=16)
 
+    cur_count += count
     match dir:
         case Dir.UP:
-            for i in range(count):
-                land[cur_pos.row_idx - i][cur_pos.col_idx] = '#'
+            for i in range(1, count):
+                land[cur_pos.row_idx - i].append((cur_pos.col_idx, cur_pos.col_idx))
+                land[cur_pos.row_idx - i].sort(key=lambda x: (x[1], x[0]))
             cur_pos = Pos(cur_pos.row_idx - count, cur_pos.col_idx)
         case Dir.LEFT:
-            for i in range(count):
-                land[cur_pos.row_idx][cur_pos.col_idx - i] = '#'
+            cur_stack_count += count
+            land[cur_pos.row_idx].append((cur_pos.col_idx - count, cur_pos.col_idx))
+            land[cur_pos.row_idx] = sorted(land[cur_pos.row_idx], key=lambda x: (x[1], x[0]))
             cur_pos = Pos(cur_pos.row_idx, cur_pos.col_idx - count)
         case Dir.DOWN:
-            for i in range(count):
-                land[cur_pos.row_idx + i][cur_pos.col_idx] = '#'
+            for i in range(1, count):
+                land[cur_pos.row_idx + i].append((cur_pos.col_idx, cur_pos.col_idx))
+                land[cur_pos.row_idx + i].sort(key=lambda x: (x[1], x[0]))
             cur_pos = Pos(cur_pos.row_idx + count, cur_pos.col_idx)
         case Dir.RIGHT:
-            for i in range(count):
-                land[cur_pos.row_idx][cur_pos.col_idx + i] = '#'
+            cur_stack_count += count
+            land[cur_pos.row_idx].append((cur_pos.col_idx, cur_pos.col_idx + count))
+            land[cur_pos.row_idx] = sorted(land[cur_pos.row_idx], key=lambda x: (x[1], x[0]))
             cur_pos = Pos(cur_pos.row_idx, cur_pos.col_idx + count)
 
     if cur_pos.col_idx < col_idx_start:
@@ -98,58 +124,52 @@ for line in input:
         row_idx_end = cur_pos.row_idx
 
 height = row_idx_end - row_idx_start + 1
-width = col_idx_end - col_idx_start + 1
-# print(row_idx_start, row_idx_end, col_idx_start, col_idx_end)
+print('populated', 'count:', cur_count)
+# print('dim', row_idx_start, row_idx_end, col_idx_start, col_idx_end)
 # print(height, width)
 
-def fill(start_row_idx, start_col_idx, land):
-    count = 0
-    q = [(start_row_idx, start_col_idx)]
 
-    while q:
-        cur_pos = q.pop(0)
-        if land[cur_pos[0]][cur_pos[1]] == '.':
-            land[cur_pos[0]][cur_pos[1]] = '/'
-        else:
-            continue
-        
-        q.append((cur_pos[0]-1, cur_pos[1]))
-        q.append((cur_pos[0], cur_pos[1]-1))
-        q.append((cur_pos[0]+1, cur_pos[1]))
-        q.append((cur_pos[0], cur_pos[1]+1))
+filled: dict[int, list[tuple[int, int]]] = defaultdict(list)
 
 # find the upper left corner
-first_row  = sorted(list(land[row_idx_start].keys()))
-fill(row_idx_start + 1, first_row[0] + 1, land)
+# first_row  = sorted(land[row_idx_start], key=lambda x: x[1])
+# print(first_row)
 
-counter = 0
-for row_idx in range(row_idx_start, row_idx_end + 1):
-    inside = False
-    print(f'{row_idx:3d}', end='')
-    edge_count = 0
-    for col_idx in range(col_idx_start, col_idx_end + 1):
-        if land[row_idx][col_idx] == '#':
-            counter += 1
-            edge_count += 1
+# Start at the 2nd row since nothing will need to be filled in
+for row_idx in range(row_idx_start+1, row_idx_end + 1):
+    row_contents = land[row_idx] 
+    if len(row_contents) <= 1:
+        continue
 
-            if not inside and land[row_idx][col_idx + 1] == '.' and [tmp_col_idx for tmp_col_idx in list(land[row_idx].keys()) if isinstance(tmp_col_idx, int) and tmp_col_idx > col_idx + 1]:
-                inside = True
-            elif inside and land[row_idx][col_idx - 1] == '.':
-                inside = False 
-            print('#', end='')
-        elif land[row_idx][col_idx] == '/':
-            print('/', end='')
-            counter += 1
-        else:
-            if col_idx > 100:
-                # break
-                pass
-            if inside and False:
-                # counter += 1
-                print('/', end='')
-            else:
-                print('.', end='')
-    print()
+    for i in range(len(row_contents) - 1):
+        fill_range = row_contents[i][1]+1, row_contents[i+1][0] - 1
+        if any(cur_prev_range for cur_prev_range in filled[row_idx-1] if range_overlaps(cur_prev_range, fill_range)) or (
+            any(cur_prev_range for cur_prev_range in land[row_idx-1] if range_overlaps(cur_prev_range, fill_range) and 
+                not any(cur_prev_range for cur_prev_range in filled[row_idx-2] if range_overlaps(cur_prev_range, fill_range)) )
+        ):
+            fill_count = fill_range[1] - fill_range[0] + 1
+            # print('row_idx:', row_idx, 'fill:', fill_range, 'fill count:', fill_count)
+            filled[row_idx].append(fill_range)
+            cur_count += fill_count
 
-    
-print('1:', counter)
+# Print out landfill plan
+# for row_idx in range(row_idx_start, row_idx_end + 1):
+#     inside = False
+#     print(f'{row_idx:3d}', end='')
+#     for col_idx in range(col_idx_start, col_idx_end + 1):
+#         if any(cur_prev_range for cur_prev_range in land[row_idx] if range_overlaps(cur_prev_range, (col_idx, col_idx))):
+#             print('#', end='')
+#         elif any(cur_prev_range for cur_prev_range in filled[row_idx] if range_overlaps(cur_prev_range, (col_idx, col_idx))):
+#             print('/', end='')
+#         else:
+#             if col_idx > 100:
+#                 # break
+#                 pass
+#             if inside and False:
+#                 # counter += 1
+#                 print('/', end='')
+#             else:
+#                 print('.', end='')
+#     print()
+
+print('2:', cur_count)
--- 18/README.md
+++ 18/README.md
@@ -1,6 +1,9 @@
 # Day 18
 
 Part 1 - I cheated by manually selecting where to fill
+
+Part 2 - The main issue is we have ranges for left/right but not up/down.
+           We can potentially optimize by copying the fill_range_count during the up/down steps 
 ## Performance
 ```bash
 # Part 1
@@ -10,4 +13,13 @@ Part 1 - I cheated by manually selecting where to fill
 real    0m0.296s
 user    0m0.277s
 sys     0m0.012s
+
+# Part 2 (attempt 1)
+(.venv) vscode ➜ /workspaces/2023/18 (main) $ time python 18b.py
+populated count: 167690176
+2: 106920098354636
+
+real    3m3.031s
+user    2m55.903s
+sys     0m7.116s
 ```
