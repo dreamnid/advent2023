4619819d4667db787badf05569231e83090fbc3b
Giovanni Go <dreamnid@dreamnid.com>
2023-12-20 07:30:28 +0000

    Day 20 part 2

--- 20/20.py
+++ 20/20.py
@@ -46,6 +46,7 @@ for line in get_file_contents(INPUT_FILE)[0]:
         modules_dest[module_name] = destinations
         for cur_dest in destinations:
             modules_in_value[cur_dest][module_name] = False
+            modules_in_src[cur_dest].append(module_name)
 
         match line_split[0][0]:
             case '%':
@@ -54,6 +55,8 @@ for line in get_file_contents(INPUT_FILE)[0]:
             case '&':
                 modules_type[module_name] = 'conj'
                 modules_value[module_name] = False
+
+# pprint.pprint(modules_in_src)
 
 def flip(input: bool, cur_value: bool):
     if input:
@@ -70,7 +73,9 @@ class Step(NamedTuple):
     input: bool
     src_module_name: str
 
-def button(break_on_rx=False):
+loop_count = defaultdict(list)
+
+def button(*, break_on_rx=False, it=None):
     process: list[Step] = deque([])
     lo_pulses = 1 # start at 1 due to button
     hi_pulses = 0
@@ -97,6 +102,12 @@ def button(break_on_rx=False):
                 # print('output', cur_input)
                 continue
 
+        if cur_module in modules_in_src['qt'] and new_value:
+            loop_count[cur_module].append(it)
+
+            if all([len(loop_count[tmp_module]) > 1 for tmp_module in modules_in_src['qt']]) and break_on_rx:
+                return None
+
         modules_value[cur_module] = new_value
         for next_dest in modules_dest[cur_module]:
             if new_value:
@@ -109,13 +120,14 @@ def button(break_on_rx=False):
 
 print('1:', reduce(mul, (map(sum, zip(*(button() for _ in range(1000)))))))
 
-# i = 1
-# while True:
-#     if button() is None:
-#         break
+# Part 2 requires us to find the number of button presses when rx is low. Since the source of rx is qt which is
+# a conjection module, rx will only be low when all the inputs are high.
+#
+# Loop until we get a None which indicates we found the cycle count for all the inputs for qt when it was True
+i = 1
+while True:
+    if button(break_on_rx=True, it=i) is None:
+        break
 
-#     i += 1
-
-# print('2:', i)
-
-
+    i += 1
+print('2:', math.lcm(*map(lambda x: x[1] - x[0], loop_count.values())))
--- 20/README.md
+++ 20/README.md
@@ -9,4 +9,13 @@ time python 20.py
 real    0m0.466s
 user    0m0.399s
 sys     0m0.172s
+
+# part 1 + 2
+@dreamnid ➜ /workspaces/advent2023/20 (main) $ time python 20.py 
+1: 879834312
+2: 243037165713371
+
+real    0m1.702s
+user    0m1.423s
+sys     0m0.120s
 ```
