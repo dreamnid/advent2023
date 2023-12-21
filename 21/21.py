f72220c59dbdd05b11f9724072b8f067bbdae080
Giovanni Go <dreamnid@dreamnid.com>
2023-12-20 23:43:31 -0800

    Day 21 Part 1

--- 21/21.py
+++ 21/21.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
         from util import *
 
 INPUT_FILE='21-input.txt'
-INPUT_FILE='21a-example.txt'
+# INPUT_FILE='21a-example.txt'
 
 input = [line for line in get_file_contents(INPUT_FILE)[0]]
 input = add_padding(input, 'X')
@@ -42,42 +42,41 @@ for row_idx, row in enumerate(input):
         start_pos = Pos(row_idx, row.index('S'))
         break
 
-q: list[list[Pos]] = deque([[start_pos]])
+q: list[set[Pos]] = deque([{start_pos}])
 visited: set[Pos] = set()
 
-steps = -1
+steps = 0
+MAX_STEPS = 64
 
-while q and steps < 6:
-    next_pos: list[Pos] = []
+while q and steps < MAX_STEPS:
+    next_pos: set[Pos] = set()
 
     for cur_pos in q.pop():
         visited.add(cur_pos)
 
-        if input[cur_pos.row_idx - 1][cur_pos.col_idx] == '.':
-            next_pos.append(Pos(cur_pos.row_idx - 1, cur_pos.col_idx))
+        if input[cur_pos.row_idx - 1][cur_pos.col_idx] in ['.', 'S']:
+            next_pos.add(Pos(cur_pos.row_idx - 1, cur_pos.col_idx))
 
-        if input[cur_pos.row_idx][cur_pos.col_idx - 1] == '.':
-            next_pos.append(Pos(cur_pos.row_idx, cur_pos.col_idx - 1))
+        if input[cur_pos.row_idx][cur_pos.col_idx - 1] in ['.', 'S']:
+            next_pos.add(Pos(cur_pos.row_idx, cur_pos.col_idx - 1))
 
-        if input[cur_pos.row_idx + 1][cur_pos.col_idx] == '.':
-            next_pos.append(Pos(cur_pos.row_idx + 1, cur_pos.col_idx))
+        if input[cur_pos.row_idx + 1][cur_pos.col_idx] in ['.', 'S']:
+            next_pos.add(Pos(cur_pos.row_idx + 1, cur_pos.col_idx))
         
-        if input[cur_pos.row_idx][cur_pos.col_idx + 1] == '.':
-            next_pos.append(Pos(cur_pos.row_idx, cur_pos.col_idx + 1))
+        if input[cur_pos.row_idx][cur_pos.col_idx + 1] in ['.', 'S']:
+            next_pos.add(Pos(cur_pos.row_idx, cur_pos.col_idx + 1))
 
     steps += 1
-    for row_idx, row in enumerate(input):
-        for col_idx, col in enumerate(row):
-            if Pos(row_idx, col_idx) in next_pos:
-                print('O', end='')
-            else:
-                print(input[row_idx][col_idx], end='')
+    if False:
+        for row_idx, row in enumerate(input):
+            for col_idx, col in enumerate(row):
+                if Pos(row_idx, col_idx) in next_pos:
+                    print('O', end='')
+                else:
+                    print(input[row_idx][col_idx], end='')
 
-        print()
-
-    print(len(next_pos))
+            print()
     q.append(next_pos) 
 
 
-
-print('1:', len(visited)) 
+print('1:', len(next_pos)) 
