9bc8833417e1169ab9b1296b8afe422920ebeb87
Giovanni Go <dreamnid@dreamnid.com>
2024-01-08 07:46:26 +0000

    Introduce junction data structure
    Not working at this stage

--- 23/23.py
+++ 23/23.py
@@ -33,6 +33,7 @@ if __name__ == "__main__":
 INPUT_FILE = "23-input.txt"
 INPUT_FILE = "23a-example.txt"
 # INPUT_FILE = "23b-example.txt"
+INPUT_FILE = "23c-example.txt"
 
 input = [line for line in get_file_contents(INPUT_FILE)[0]]
 
@@ -42,15 +43,34 @@ class Pos(NamedTuple):
     col: int
 
 
+Path = deque[Pos]
+
+
 class Data(NamedTuple):
-    cur_pos: Pos
-    path: deque[Pos]
+    pos: Pos
+    path: Path
+    last_split_path: Path
 
 
-def next_node(cur_data: Data, next_pos: Pos, queue: deque[Data]):
+class Junction(NamedTuple):
+    paths: deque[Path]
+    final_path: deque[Path] | None = None
+
+
+def next_node(
+    cur_data: Data, next_pos: Pos, next_last_split_path: Path, queue: deque[Data]
+):
     next_path = copy(cur_data.path)
-    next_path.append(cur_data.cur_pos)
-    queue.append(Data(next_pos, next_path))
+    next_path.append(cur_data.pos)
+    queue.append(Data(next_pos, next_path, next_last_split_path))
+
+
+def next_junction(cur_data: Data, path_segment: Path, queue: deque[Data]):
+    print("next junction", cur_data.pos, path_segment)
+    next_path = copy(cur_data.path) + path_segment
+    compare_puzzle_paths(path_segment, cur_data.path, cur_data.pos)
+    queue.append(Data(path_segment[-1], next_path, None))
+    pass
 
 
 def get_input_value(cur_pos: Pos):
@@ -96,107 +116,124 @@ def solver(is_slopes_slippery=True):
     Set is_slopes_slippey to True for part 1
     """
     completed: deque[Data] = deque()
-    queue: deque[Data] = deque([Data(Pos(1, 1), deque([Pos(0, 1)]))])
-    memo: dict[Pos, dict[Pos, int]] = defaultdict(lambda: defaultdict(int))
-    memo_path: dict[Pos, dict[Pos, Iterable[Pos]]] = defaultdict(dict)
-    splits: dict[Pos, int] = dict()
+    queue: deque[Data] = deque([Data(Pos(1, 1), deque([Pos(0, 1)]), None)])
+    junctions: dict[Pos, Junction] = dict()
+    # paths: dict[Pos, Path] = dict()
     bad_pos: set(Pos) = set()
     bad_path_pos: dict[Pos, set[Pos]] = defaultdict(set)
-    bad_path_pos[Pos(131, 132)].add(Pos(131, 133))
+    start_path_to_first_junction: deque[Path] = deque()
+    first_junction: Junction = None
     idx = 0
 
     while queue:
         idx += 1
         cur_data = queue.pop()
-        path_len = len(cur_data.path)
-        if cur_data.cur_pos in memo:
-            pass
-        if path_len and path_len < memo[cur_data.cur_pos][cur_data.path[-1]]:
-            # print_puzzle(cur_data.path, cur_pos=cur_data.cur_pos)
-            # print_puzzle(memo_path[cur_data.cur_pos], cur_pos=cur_data.cur_pos)
-            compare_puzzle_paths(
-                cur_data.path,
-                memo_path[cur_data.cur_pos][cur_data.path[-1]],
-                cur_pos=cur_data.cur_pos,
-            )
-            continue
+        cur_pos = cur_data.pos
 
-        if path_len > 1 and cur_data.path[-1] in bad_path_pos[cur_data.cur_pos]:
-            # This path leads to a dead end
-            print("bad")
-            continue
-
-        cur_val = get_input_value(cur_data.cur_pos)
+        cur_val = get_input_value(cur_pos)
         if not is_slopes_slippery and cur_val in (">", "<", "v", "^"):
             cur_val = "."
 
         match cur_val:
             case ".":
-                if cur_data.cur_pos == final_pos:
+                if cur_pos == final_pos:
                     completed.append(cur_data)
+                    last_junction = cur_data.last_split_path.popleft()
+                    junctions[last_junction] = junctions[last_junction]._replace(
+                        final_path=cur_data.last_split_path
+                    )
                     print_puzzle(cur_data.path)
-                    total_len = len(cur_data.path)
-                    # Update memoization
-                    prev_pos = None
-                    last_split = None
-                    for tmp_idx, tmp_pos in enumerate(cur_data.path, 0):
-                        if tmp_idx == 0:
-                            prev_pos = tmp_pos
+                    continue
+                elif cur_pos in junctions:
+                    # We've been to this junction before
+                    if junctions[cur_pos].final_path:
+                        cur_data.path.extend(junctions[cur_pos].final_path)
+                        completed.append(cur_data)
+                        continue
+
+                    for junction_path in junctions[cur_pos].paths:
+                        if junction_path[-1] in cur_data.path:
                             continue
-                        if tmp_pos in splits:
-                            last_split = tmp_pos
-
-                        if tmp_idx > memo[tmp_pos][prev_pos]:
-                            memo[tmp_pos][prev_pos] = tmp_idx
-                            memo_path[tmp_pos][prev_pos] = cur_data.path
-                        prev_pos = tmp_pos
-
+                        if not junction_path:
+                            continue
+                        tmp_max_path = copy(junction_path)
+                        tmp_max_path.popleft()
+                        next_junction(cur_data, tmp_max_path, queue)
                     continue
                 neighbors = [
-                    cur_data.cur_pos._replace(row=cur_data.cur_pos.row - 1),
-                    cur_data.cur_pos._replace(col=cur_data.cur_pos.col - 1),
-                    cur_data.cur_pos._replace(row=cur_data.cur_pos.row + 1),
-                    cur_data.cur_pos._replace(col=cur_data.cur_pos.col + 1),
+                    cur_pos._replace(row=cur_pos.row - 1),
+                    cur_pos._replace(col=cur_pos.col - 1),
+                    cur_pos._replace(row=cur_pos.row + 1),
+                    cur_pos._replace(col=cur_pos.col + 1),
                 ]
             case ">":
-                neighbors = [cur_data.cur_pos._replace(col=cur_data.cur_pos.col + 1)]
+                neighbors = [cur_data.cur_pos._replace(col=cur_pos.col + 1)]
             case "<":
-                neighbors = [cur_data.cur_pos._replace(col=cur_data.cur_pos.col - 1)]
+                neighbors = [cur_data.cur_pos._replace(col=cur_pos.col - 1)]
             case "v":
-                neighbors = [cur_data.cur_pos._replace(row=cur_data.cur_pos.row + 1)]
+                neighbors = [cur_data.cur_pos._replace(row=cur_pos.row + 1)]
             case "^":
-                neighbors = [cur_data.cur_pos._replace(row=cur_data.cur_pos.row - 1)]
+                neighbors = [cur_data.cur_pos._replace(row=cur_pos.row - 1)]
 
-        added_new_node = 0
-        for cur_neighbor in neighbors:
+        valid_neighbors = [
+            cur_neighbor
+            for cur_neighbor in neighbors
             if (
                 cur_neighbor not in cur_data.path
                 and get_input_value(cur_neighbor) != "#"
-            ):
-                next_node(cur_data, cur_neighbor, queue)
-                added_new_node += 1
+            )
+        ]
+        match len(valid_neighbors):
+            case 0:
+                # detect if deadend
+                if (
+                    len([val for val in map(get_input_value, neighbors) if val == "#"])
+                    == 3
+                ):
+                    prev_pos = None
+                    for tmp_pos in reversed(cur_pos):
+                        if tmp_pos in junctions:
+                            bad_path_pos[prev_pos].add(tmp_pos)
+                            break
+                        prev_pos = tmp_pos
+                    cur_data.last_split_path.clear()
+                    # print_puzzle(cur_data.path, cur_data.cur_pos)
+                    for tmp_pos in reversed(cur_pos):
+                        if tmp_pos in junctions:
+                            break
+                        bad_pos.add(cur_pos)
+                continue
+            case 1:
+                if cur_data.last_split_path:
+                    cur_data.last_split_path.append(cur_pos)
+                else:
+                    start_path_to_first_junction.append(cur_pos)
+                next_node(cur_data, valid_neighbors[0], cur_data.last_split_path, queue)
+            case _:
+                # New Junction
+                cur_junction = Junction(paths=deque())
+                junctions[cur_pos] = cur_junction
+                if not first_junction:
+                    first_junction = cur_junction
+                    start_path_to_first_junction.append(cur_pos)
+                # Add recipricol path
+                if cur_data.last_split_path:
+                    cur_data.last_split_path.append(cur_pos)
+                    cur_junction.paths.append(deque(reversed(cur_data.last_split_path)))
 
-        if added_new_node > 1:
-            splits[cur_data.cur_pos] = added_new_node
-        elif not added_new_node:
-            # detect if deadend
-            if len([val for val in map(get_input_value, neighbors) if val == "#"]) == 3:
-                prev_pos = None
-                for tmp_pos in reversed(cur_data.cur_pos):
-                    if tmp_pos in splits:
-                        bad_path_pos[prev_pos].add(tmp_pos)
-                        break
-                    prev_pos = tmp_pos
-            # print_puzzle(cur_data.path, cur_data.cur_pos)
-            for tmp_pos in reversed(cur_data.cur_pos):
-                if tmp_pos in splits:
-                    break
-                bad_pos.add(cur_data.cur_pos)
+                # Add remaining paths
+                for cur_neighbor in valid_neighbors:
+                    new_path = deque([cur_pos])
+                    cur_junction.paths.append(new_path)
+                    next_node(cur_data, cur_neighbor, new_path, queue)
 
-        if idx % 200 == 0 and False:
+        if idx % 1 == 0 or True:
             print(f"idx {idx}")
-            print_puzzle(cur_data.path, cur_data.cur_pos)
-    pprint.pprint(memo)
+            print_puzzle(cur_data.path, cur_pos)
+    # pprint.pprint(junctions[Pos(1, 1)].paths)
+    # pprint.pprint(junctions[Pos(1, 1)].paths)
+    print("*" * 30)
+    # pprint.pprint(junctions[Pos(3, 2)].paths)
     return completed
 
 
@@ -212,4 +249,5 @@ completed = sorted(
 print([len(x.path) for x in completed])
 # pprint.pprint(completed)
 print_puzzle(completed[0].path)
+pprint.pprint(completed[0].path)
 print("2", len(completed[0].path))
